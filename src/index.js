a6b81ad468fd6dff9134ba27e45a717dee68bfe2
Michael Daley <mgd3@njit.edu>
2024-07-16 17:24:18 +0900

    implemented page tabbing system

--- src/index.js
+++ src/index.js
@@ -1,4 +1,50 @@
-import './css/style.css' 
-import home from './modules/home.js'
-import menu from './modules/menu.js'
-import about from './modules/about.js'
+import './css/style.css';
+import home from './modules/home.js';
+import menu from './modules/menu.js';
+import about from './modules/about.js';
+import icon from './images/ice cream cone.svg';
+
+let logo = document.getElementById('logo');
+
+const myImg = new Image();
+myImg.src = icon;
+logo.prepend(myImg);
+
+const tabManager = (() => {
+
+    const homeBtn = document.getElementById('home-btn');
+    const menuBtn = document.getElementById('menu-btn');
+    const aboutBtn = document.getElementById('about-btn');
+    let currPage = null;
+
+    const clearPage = () => {
+        const content = document.getElementById('content');
+        if(content.firstChild) content.removeChild(content.firstChild);
+        currPage = null;
+        
+    }
+
+    const setPage = (btn, pageFunc) => {
+        let pageId = btn.id;
+        if(currPage === pageId){
+            return;
+        }
+        if(currPage){
+            clearPage();
+        }
+        pageFunc();
+        currPage = pageId;
+    }
+
+    homeBtn.addEventListener('click', () => {
+        setPage(homeBtn,home);
+    });
+
+    menuBtn.addEventListener('click', () => {
+        setPage(menuBtn, menu);
+    });
+
+    aboutBtn.addEventListener('click', () => {
+        setPage(aboutBtn, about);
+    });
+})();
--- src/modules/home.js
+++ src/modules/home.js
@@ -1,4 +1,11 @@
-const home_module = (div) => {
+import mainImg from '../images/main logo.png';
+const home_module = () => {
+    const content = document.getElementById('content');
+    const homeContainer = document.createElement('div')
+    const image = document.createElement('img');
+    image.src = mainImg;
+    homeContainer.appendChild(image);
+    content.appendChild(homeContainer);
 
 }
 
